ab6ad61e0a30ae98b0b715fd8939598cce9639eb
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2023-11-13 19:36:37 -0300

    13/11/2023

--- paginas_web/app_com_firebase/js/script.js
+++ paginas_web/app_com_firebase/js/script.js
@@ -8,8 +8,9 @@ const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 addButtonEl.addEventListener("click", function () {
     let inputValue = inputFieldEl.value
-    push(shoppingListInDB, inputValue)
-    console.log(`${inputValue} added to database`)
+    push(shoppingListInDB, inputValue);
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`;
 })
