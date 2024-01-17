3182b674005c8fc837d2de28488d439f80910499
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-01-17 09:22:56 -0300

    17/01/2024

--- Páginas Web/HTML/Login com Firebase/js/home.js
+++ Páginas Web/HTML/Login com Firebase/js/home.js
@@ -5,3 +5,56 @@ function Logout() {
         alert('Erro ao fazer logout');
     })
 }
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        FindTransactions(user)
+    }
+})
+function NewTransaction() {
+    window.location.href = "./transactions.html";
+}
+function FindTransactions(user) {
+    ShowLoading();
+    firebase.firestore()
+        .collection('transactions')
+        .where('user.uid', '==', user.uid)
+        .orderBy('date', 'desc')
+        .get()
+        .then(snapshot => {
+            HideLoading();
+            const transactions = snapshot.docs.map(doc => doc.data());
+            AddTransactionsToScreen(transactions);
+        }).catch(error => {
+            HideLoading();
+            console.log(error);
+            alert('Erro ao recuperar transações');
+        })
+}
+function AddTransactionsToScreen(transactions) {
+    const orderedList = document.getElementById('transactions');
+    transactions.forEach(transaction => {
+        const li = document.createElement('li');
+        li.classList.add(transaction.type);
+        const date = document.createElement('p');
+        date.innerHTML = FormatData(transaction.date);
+        li.appendChild(date);
+        const money = document.createElement('p');
+        money.innerHTML = FormatMoney(transaction.money);
+        li.appendChild(money);
+        const type = document.createElement('p');
+        type.innerHTML = transaction.transactionType;
+        li.appendChild(type);
+        if (transaction.description) {
+            const description = document.createElement('p');
+            description.innerHTML = transaction.description;
+            li.appendChild(description);
+        }
+        orderedList.appendChild(li);
+    });
+}
+function FormatData(date) {
+    return new Date(date).toLocaleDateString('pt-br');
+}
+function FormatMoney(money) {
+    return `${money.currency} ${money.value.toFixed(2)}`
+}
